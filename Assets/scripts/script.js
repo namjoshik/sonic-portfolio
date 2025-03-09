f18f32f7b456cae32dc00f88f07d220ad616cde0
namjoshik <42796001+namjoshik@users.noreply.github.com>
2025-03-09 21:38:42 +0400

    Site overhaul

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -3,3 +3,15 @@ const about_me_link = document.getElementById('_aboutMe');
 about_me_link.onclick = () => {
   console.log('About Me is clicked');
 };
+
+const skills_css = document.getElementById('skills');
+
+window.onresize = () => {
+  console.log(window.innerWidth);
+  if (window.innerWidth <= 992) {
+    skills_css.style =
+      'border-bottom: 0.75px solid black; margin-bottom: 10px;';
+  } else {
+    skills_css.style = 'border-right: 0.75px solid black;';
+  }
+};
